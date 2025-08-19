7af44209649978b33cae0b976c374377377358f9
Mégane Vilain <megane.vilain@gmail.com>
2025-08-19 21:44:16 +0200

    Add logging to file

--- utils/logging_config.py
+++ utils/logging_config.py
@@ -1,8 +1,16 @@
 import logging
 
 def init_logging(level=logging.INFO):
+    level = logging.INFO
+    log_format = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
+
     logging.basicConfig(
         level=level,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
+        format=log_format,
+        datefmt=date_format,
+        handlers=[
+            logging.StreamHandler(),  # console
+            logging.FileHandler("app.log", encoding="utf-8")  # file
+        ]
     )
